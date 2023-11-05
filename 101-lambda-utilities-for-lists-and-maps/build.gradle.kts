faef43c922065eaddc59328654981ab129862e01
pianinator <113355583+pianinator@users.noreply.github.com>
2023-11-05 18:37:53 +0000

    Deploying to solutions from @ APICe-at-DISI/exercises@1c2dcb40768bec0b1a9e42763172db95b6668c81 🚀

--- 101-lambda-utilities-for-lists-and-maps/build.gradle.kts
+++ 101-lambda-utilities-for-lists-and-maps/build.gradle.kts
@@ -15,10 +15,10 @@ repositories {
 
 dependencies {
     // Use JUnit Jupiter API for testing.
-    testImplementation("org.junit.jupiter:junit-jupiter-api:5.10.0")
+    testImplementation("org.junit.jupiter:junit-jupiter-api:5.10.1")
 
     // Use JUnit Jupiter Engine for testing.
-    testRuntimeOnly("org.junit.jupiter:junit-jupiter-engine:5.10.0")
+    testRuntimeOnly("org.junit.jupiter:junit-jupiter-engine:5.10.1")
 }
 
 val mainClass: String by project
