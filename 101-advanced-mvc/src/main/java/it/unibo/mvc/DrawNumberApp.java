cf8ddc46cee22c0f8e67d5d024d37f2b3e51a234
pianinator <113355583+pianinator@users.noreply.github.com>
2023-11-17 09:38:56 +0000

    Deploying to exercises from @ APICe-at-DISI/exercises@491d04f3410f794a4c2e69dc70346e5663cff590 🚀

---
+++ 101-advanced-mvc/src/main/java/it/unibo/mvc/DrawNumberApp.java
@@ -0,0 +1,72 @@
+package it.unibo.mvc;
+
+import java.io.FileNotFoundException;
+import java.util.Arrays;
+import java.util.List;
+
+/**
+ */
+public final class DrawNumberApp implements DrawNumberViewObserver {
+    private static final int MIN = 0;
+    private static final int MAX = 100;
+    private static final int ATTEMPTS = 10;
+
+    private final DrawNumber model;
+    private final List<DrawNumberView> views;
+
+    /**
+     * @param views
+     *            the views to attach
+     */
+    public DrawNumberApp(final DrawNumberView... views) {
+        /*
+         * Side-effect proof
+         */
+        this.views = Arrays.asList(Arrays.copyOf(views, views.length));
+        for (final DrawNumberView view: views) {
+            view.setObserver(this);
+            view.start();
+        }
+        this.model = new DrawNumberImpl(MIN, MAX, ATTEMPTS);
+    }
+
+    @Override
+    public void newAttempt(final int n) {
+        try {
+            final DrawResult result = model.attempt(n);
+            for (final DrawNumberView view: views) {
+                view.result(result);
+            }
+        } catch (IllegalArgumentException e) {
+            for (final DrawNumberView view: views) {
+                view.numberIncorrect();
+            }
+        }
+    }
+
+    @Override
+    public void resetGame() {
+        this.model.reset();
+    }
+
+    @Override
+    public void quit() {
+        /*
+         * A bit harsh. A good application should configure the graphics to exit by
+         * natural termination when closing is hit. To do things more cleanly, attention
+         * should be paid to alive threads, as the application would continue to persist
+         * until the last thread terminates.
+         */
+        System.exit(0);
+    }
+
+    /**
+     * @param args
+     *            ignored
+     * @throws FileNotFoundException 
+     */
+    public static void main(final String... args) throws FileNotFoundException {
+        new DrawNumberApp(new DrawNumberViewImpl());
+    }
+
+}
